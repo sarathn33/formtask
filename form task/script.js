affdb35e4cbad659c2c52630a8d33af6ef6751c7
Sarath N <115686647+sarathn33@users.noreply.github.com>
2023-01-03 19:26:57 +0530

    Add files via upload

---
+++ form task/script.js
@@ -0,0 +1,47 @@
+const form = document.getElementById('form');
+const table = document.getElementById('table');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const firstName = document.getElementById('first-name').value;
+  const lastName = document.getElementById('last-name').value;
+  const email = document.getElementById('email').value;
+  const address = document.getElementById('address').value;
+  const pincode = document.getElementById('pin').value;
+  const gender = document.querySelector('input[name="gender"]:checked').value;
+
+  var checkboxes = form.querySelectorAll('input[name="food"]');
+  var checkedCount = Array.from(checkboxes).filter((food) => food.checked).length;
+  if (checkedCount < 2) {
+    event.preventDefault();
+    alert('Please select at least two options');
+  }else{
+
+  var food = [];
+  var foodElements = document.querySelectorAll('input[name="food"]:checked');
+  for (var element of foodElements) {
+    food.push(element.value);
+  }
+}
+  const state = document.getElementById('state').value;
+  const country = document.getElementById('countries').value;
+  const comments=document.getElementById('comments').value;
+  const row = document.createElement('tr');
+  row.innerHTML = `
+    <td>${firstName}</td>
+    <td>${lastName}</td>
+    <td>${email}</td>
+    <td>${address}</td>
+    <td>${pincode}</td>
+    <td>${gender}</td>
+    <td>${food.join(', ')}</td>
+    <td>${state}</td>
+    <td>${country}</td>
+    <td>${comments}</td>
+  `;
+
+  table.appendChild(row);
+
+  form.reset();
+});
